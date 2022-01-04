530b27ca768648b22cd200149ca6c639ae61471c
rikako <jc180215b@gmail.com>
2022-01-04 23:19:59 +0900

    modify:admin add

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,13 +1,21 @@
 class Admin::UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
-  def edit
-  end
+  def create
+    @user = User.new(user_params)
 
-  def show
+    if @user.save
+      redirect_to admin_users_path, notice: "ユーザー「#{@user.name}」を登録いたしました。"
+    else
+      render :new
+    end
   end
+  
+  private
 
-  def index
+  def user_params
+    params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
   end
 end
