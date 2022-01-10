d30acf014ae936b3a30e95f138bb066aad7da4ab
rikako <jc180215b@gmail.com>
2022-01-10 13:46:10 +0900

    modify:systemSpec

---
+++ spec/system/tasks_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'タスク管理機能', type: :system do
+  describe '一覧表示機能', do
+    before do 
+      user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
+      FactoryBot.create(:task, name: '最初のタスク', user: user_a)
+  end
+
+  context 'ユーザーAがログインしているとき' do
+      before do
+        visit login_path
+        fill_in 'メールアドレス', with 'a@example.com'
+        fill_in 'パスワード', with: 'password'
+        click_button 'ログインする'
+      end
+  end
+end
